24323f478838b637160224e17e72265972e3e71e
Mat Bentley <mathewkbentley@gmail.com>
2017-09-12 01:09:25 -0400

    refactoring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,19 @@ const gulpmodules = ['./src_modules/common',
                      './src_modules/client',
                      './src_modules/server'];
 
-function moduleStart(taskNames) {
-  gulpmodules.forEach((dir) => {
-    process.chdir(path.join(__dirname, dir));
-    const child = spawn(process.platform === 'win32' ? 'gulp.cmd' : 'gulp', taskNames, { customFds: [0,1,2] });
-    child.on('exit', (text) => {
-      console.log(text);
+function moduleTask(taskNames) {
+  return () => {
+    gulpmodules.forEach((dir) => {
+      process.chdir(path.join(__dirname, dir));
+      const child = spawn(process.platform === 'win32' ? 'gulp.cmd' : 'gulp', taskNames, { customFds: [0,1,2] });
+      child.on('exit', (text) => {
+        console.log(text);
+      });
     });
-  });
+  };
 };
 
-gulp.task('default', () => {
-  moduleStart(['default']);
-});
-gulp.task('test', () => {
-  moduleStart(['test']);
-});
-gulp.task('clean', () => {
-  moduleStart(['clean']);
-});
-gulp.task('build', () => {
-  moduleStart(['build']);
-});
+gulp.task('default', moduleTask(['default']));
+gulp.task('test', moduleTask(['test']));
+gulp.task('clean', moduleTask(['clean']));
+gulp.task('build', moduleTask(['build']));
